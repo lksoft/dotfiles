f7bcbf2fd8c2922ba67c5da996c247a15ed768fe
Scott Little <scott@littleknownsoftware.com>
2018-10-13 19:46:47 +0100

    Add the AWS config stuff

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -5,12 +5,8 @@ if [ "$1" = "" ]; then
 	export MISSING_VALUE="yes"
 fi
 
-if [ "$2" = "" ]; then
-	export MISSING_VALUE="yes"
-fi
-
 if [ "$MISSING_VALUE" = "yes" ]; then
-	echo "Usage: setup # (pm|cb)i [#], where the first # is the server number and the second value indicates the type of server, while the optional last number indicates the number of that type."
+	echo "Usage: setup # [pm|cb] [#], where the first # is the server number and the second value indicates the type of server, while the optional last number indicates the number of that type."
 	exit 0
 fi
 
@@ -19,13 +15,17 @@ if [ $(id -u) -ne 0 ]; then
 	exit 0
 fi
 
+export TYPE=""
 export SUPPLEMENTAL_NAME=""
-if [ "$3" != "" ]; then
-	export SUPPLEMENTAL_NAME="-${2}${3}"
+if [ "$2" != "" ]; then
+	export TYPE="-${2}"
+	if [ "$3" != "" ]; then
+		export SUPPLEMENTAL_NAME="-${2}${3}"
+	fi
 fi
 
 # Update apache with the pre Let's Encrypt server config and restart
-sed -e s/XX/${1}/g configure/pre-le-apache.conf > apache-${1}.conf
+sed -e s/XX/${1}/g ~/.aws_conf/pre-le-apache.conf > apache-${1}.conf
 mv apache-${1}.conf /etc/apache2/sites-available/000-default.conf
 apachectl -k restart
 
@@ -34,12 +34,12 @@ certbot certonly --webroot -w /var/www/lets.smallcubed.com -d sc${1}.smallcubed.
 certbot renew
 
 # Update the apachec config and restart again
-sed -e s/XX/${1}/g configure/apache-${2}> apache-${1}.conf
+sed -e s/XX/${1}/g ~/.aws_conf/apache${TYPE}> apache-${1}.conf
 mv apache-${1}.conf /etc/apache2/sites-available/000-default.conf
 apachectl -k restart
 
 # Rewrite the webmin config to use the correct cert file & restart
-sed -e s/scXX/sc${1}/g configure/webmin.conf > webmin-${1}.conf
+sed -e s/scXX/sc${1}/g ~/.aws_conf/webmin.conf > webmin-${1}.conf
 mv webmin-${1}.conf /etc/webmin/miniserv.conf
 /etc/init.d/webmin restart
 
